2024b989f1b43b4c8716542500dfda1b9adf18ef
Ori Gold <nehoraigold@gmail.com>
2018-11-05 17:06:54 +0200

    fixed bug in upper left corner

--- js/paint.js
+++ js/paint.js
@@ -5,7 +5,6 @@ var paintbrush = {
 }
 var body = document.getElementsByTagName('body')[0];
 var canvas = document.getElementById('canvas');
-
 var mousedown = false;
 
 function mouseHold(event) {
@@ -18,26 +17,25 @@ function mouseHold(event) {
 
 function paint(obj) {
     if (mousedown) {
-        var droplet = document.createElement("div");
-        droplet.style.backgroundColor = paintbrush.color;
-        droplet.style.width = paintbrush.size + "px";
-        droplet.style.height = paintbrush.size + "px";
-        if (paintbrush.shape === "circle") {
-            droplet.style.borderRadius = "50%";
+        if (obj.target.id === "canvas") {
+            var droplet = document.createElement("div");
+            droplet.style.backgroundColor = paintbrush.color;
+            droplet.style.width = paintbrush.size + "px";
+            droplet.style.height = paintbrush.size + "px";
+            if (paintbrush.shape === "circle") {
+                droplet.style.borderRadius = "50%";
+            }
+            droplet.style.position = "absolute";
+            droplet.style.left = (obj.offsetX) + "px";
+            droplet.style.top = (obj.offsetY) + "px";
+            canvas.appendChild(droplet);
         }
-        droplet.style.position = "absolute";
-        droplet.style.left = obj.offsetX + "px";
-        droplet.style.top = obj.offsetY + "px";
-        canvas.appendChild(droplet);
     }
 }
 
-function clearCanvas() {
-    canvas.innerHTML = "";
-}
-
-document.getElementById('clear-canvas').addEventListener('click',clearCanvas)
-canvas.addEventListener('mousedown',paint)
+document.getElementById('clear-canvas').addEventListener('click', function() {canvas.innerHTML = ""})
+canvas.addEventListener('click',paint)
 canvas.addEventListener('mousemove',paint)
 body.addEventListener('mousedown',mouseHold);
 body.addEventListener('mouseup',mouseHold);
+canvas.addEventListener('click',mouseHold)
